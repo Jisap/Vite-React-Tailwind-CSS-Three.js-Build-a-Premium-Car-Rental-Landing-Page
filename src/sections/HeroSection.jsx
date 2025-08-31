f4fa4006fae7c40b3aa5a289538151def78fdf3a
Jorge Matos <jisapjisap@gmail.com>
2025-08-31 17:02:29 +0200

    Revamp HeroSection component: restructure layout, add headings and descriptive text, and incorporate icons for enhanced visual appeal

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,8 +1,40 @@
+import { Sparkle, Sparkles } from 'lucide-react'
 import React from 'react'
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
+    <section>
+      <div>
+        <div>
+          {/* left */}
+          <div>
+            <div>
+              {/* sub head */}
+              <div>
+                <div>
+                  <Sparkles /> Your Journey, Elevated
+                </div>
+              </div>
+
+              {/* Main head */}
+              <h1>
+                <span>Premium</span> car, <br/> rental
+              </h1>
+            </div>
+          </div>
+
+          {/* right */}
+          <div>
+            <p>
+              Unlock unparalleled freedom with our premium car rental service. <br/>
+              Experience luxury, comfort, and seamless booking.
+             </p>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
   )
 }
 
