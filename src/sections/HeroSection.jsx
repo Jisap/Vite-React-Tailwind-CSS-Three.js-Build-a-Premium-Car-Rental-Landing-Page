320ba53a901043a5f7aa650400d2812a466ab965
Jorge Matos <jisapjisap@gmail.com>
2025-08-31 21:24:03 +0200

    Update HeroSection styling: enhance text presentation, add icon to subheading, and improve layout for better readability

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -13,21 +13,21 @@ const HeroSection = () => {
               {/* sub head */}
               <div className='mb-4'>
                 <div className='bg-zinc-300/20 text-zinc-50 rounded-full px-4 py-1 text-sm font-medium inline-flex items-center clash-display'>
-                  <Sparkles /> Your Journey, Elevated
+                  <Sparkles className='h-3.5 w-3.5 mr-1' /> Your Journey, Elevated
                 </div>
               </div>
 
               {/* Main head */}
-              <h1>
-                <span>Premium</span> car, <br/> rental
+              <h1 className='text-5xl md:text-7xl lg:text-8xl font-bold lg:leading-20 mb-6 clash-display text-nowrap tracking-tight'>
+                <span className='grad1 clash-display'>Premium</span> car, <br/> rental
               </h1>
             </div>
           </div>
 
           {/* right */}
-          <div>
-            <p>
-              Unlock unparalleled freedom with our premium car rental service. <br/>
+          <div className='right'>
+            <p className='text-lg z-10 text-gray-300 mb-8 max-w-md'>
+              Unlock unparalleled freedom with our premium car rental service. <br className='hidden md:block'/>
               Experience luxury, comfort, and seamless booking.
              </p>
           </div>
