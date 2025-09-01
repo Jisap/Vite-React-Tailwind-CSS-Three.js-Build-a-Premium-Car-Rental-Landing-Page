6cd78aba46c95078b271862ab3e1c88575f449f5
Jorge Matos <jisapjisap@gmail.com>
2025-09-01 13:37:09 +0200

    Update ServiceSection styling: enhance layout, add responsive design, and improve text presentation for better user experience

--- src/sections/ServiceSection.jsx
+++ src/sections/ServiceSection.jsx
@@ -4,27 +4,27 @@ import { Link } from "react-router-dom"
 
 const ServiceSection = () => {
   return (
-    <section className="h-screen">
-      <div>
+    <section className="section-padding bg-black">
+      <div className="max-w-7xl mx-auto">
         {/* Heading */}
-        <div>
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-12">
           <div>
-            <h2>
+            <h2 className="heading-2 clash-display grad1 from-zinc-50">
               Our Premiun Fleet & <br/> Services
             </h2>
-            <p>
+            <p className="text-zinc-400 max-w-lg">
               Discover the perfect vehicle for your needs, backed by unparalleled service and support.
             </p>
           </div>
 
-          <Link to="/fleet">
+          <Link to="/fleet" className="mt-4 sm:mt-0 flex items-center text-sky-500 hover:">
               View All Cars <MoveRight />
           </Link>
         </div>
 
         {/* Service Cards */}
         <div>
-          
+
         </div>
 
       </div>
