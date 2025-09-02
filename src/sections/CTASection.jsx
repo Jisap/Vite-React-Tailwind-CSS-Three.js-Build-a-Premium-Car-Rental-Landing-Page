561c6d95e3c4355c0d8c9750b14facfbe8116156
Jorge Matos <jisapjisap@gmail.com>
2025-09-02 15:23:16 +0200

    Enhance CTASection: implement dynamic toggler buttons with state management for improved user interaction and visual feedback.

--- src/sections/CTASection.jsx
+++ src/sections/CTASection.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import BookNowButton from '../components/BookNowButton'
+import { togglerBtns } from '../assets/assets'
 
 const CTASection = () => {
+
+  const [activeIndex, setActiveIndex] = useState(0)
+
   return (
     <>
       {/* Section Togglers */}
-      <div>
-        <div>
-
-        </div>
+      <div className='w-full h-[200px] bg-zinc-950 flex overflow-x-scroll lg:overflow-clip snap-x snap-mandatory'>
+        {togglerBtns.map((item, index) => (
+          <div 
+            key={index} 
+            className={`min-w-full center-item md:min-w-1/2 lg:min-w-1/4 h-full cursor-pointer hover:bg-zinc-800 transition-all duration-300 ease-out py-10 px-8
+            ${activeIndex === index ? "bg-zinc-200 text-zinc-700" : "bg-zinc-950" }`}
+          >
+            <div className='centered-row min-w-full gap-4 text-lg'>
+              {item.icon}
+              <p className='leading-5'>
+                {item.name}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
 
       {/* CTA Section */}
