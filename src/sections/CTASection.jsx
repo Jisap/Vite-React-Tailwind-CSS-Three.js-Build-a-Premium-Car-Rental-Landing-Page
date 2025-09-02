6996c971e43e337c73994b19c0ad1eea0745cec7
Jorge Matos <jisapjisap@gmail.com>
2025-09-02 15:41:03 +0200

    Refactor CTASection: enhance layout with gradient backgrounds, improve text presentation, and add dynamic click functionality to toggler buttons for better user interaction.

--- src/sections/CTASection.jsx
+++ src/sections/CTASection.jsx
@@ -12,6 +12,7 @@ const CTASection = () => {
       <div className='w-full h-[200px] bg-zinc-950 flex overflow-x-scroll lg:overflow-clip snap-x snap-mandatory'>
         {togglerBtns.map((item, index) => (
           <div 
+            onClick={() => setActiveIndex(index)}
             key={index} 
             className={`min-w-full center-item md:min-w-1/2 lg:min-w-1/4 h-full cursor-pointer hover:bg-zinc-800 transition-all duration-300 ease-out py-10 px-8
             ${activeIndex === index ? "bg-zinc-200 text-zinc-700" : "bg-zinc-950" }`}
@@ -27,33 +28,36 @@ const CTASection = () => {
       </div>
 
       {/* CTA Section */}
-      <section>
-        <div>
-          <div>
-
+      <section className='section-padding bg-gradient-to-b from-zinc-700/70 to-zinc-900 py-32 relative overflow-hidden'>
+        <div className='absolute inset-0 bg-transparent overflow-hidden'>
+          <div className='absolute hidden lg:block bg-transparent inset-0'>
+            {/* 3d canvas component */}
           </div>
-          <div></div>
-          <div></div>
+
+          {/* Decor: dide Overlays */}
+          <div className='absolute h-full w-20 md:w-40 lg:w-80 lg:pointer-events-none bg-gradient-to-r from-zinc-950 to-transparent'></div>
+          <div className='absolute right-0 h-full w-20 md:w-40 lg:w-80 lg:pointer-events-none bg-gradient-to-l from-zinc-950 to-transparent'></div>
         </div>
 
         {/* Text overlay */}
-        <div>
+        <div className='max-w-7xl mx-auto relative z-10 flex justify-between pointer-events-none'>
           <div>
-            <h2>
+            <h2 className='heading-2 clash-display max-w-2xl pointer-events-none'>
               View Closely Your Perfect Ride in 3D.
             </h2>
-            <p>
+            <p className='text-gray-300 max-w-md pointer-events-none mb-8'>
               Browse our expensive fleet in 3D, book with ease, and embark on your next unforgettable journey.
             </p>
             <BookNowButton type={2} />
           </div>
 
-          <div>
-            <div>
-              <p>
+          {/* Discount banner */}
+          <div className='hidden lg:block w-50 h-40 bg-blue-400/40 text-zinc-200 p-6 px-8'>
+            <div className='col'>
+              <p className='heading-2 clash-display font-semibold'>
                 50%
               </p>
-              <p>
+              <p className='text-xl leading-tight clash-display'>
                 For everyone Brand cars
               </p>
             </div>
